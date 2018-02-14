b89c7943b457f7c579649dd302b5d2da1e4aa1f4
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-14 22:39:26 +0100

    Add mock funding endpoint

---
+++ app/funding/controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify, request
+
+from app.funding.service import get_funding_by_state
+
+funding_controller = Blueprint('funding', __name__)
+
+
+@funding_controller.route('/')
+def funding_per_state_and_year():
+    year = request.args.get('year', 1995, int)
+    data = get_funding_by_state(year)
+    return jsonify(data)

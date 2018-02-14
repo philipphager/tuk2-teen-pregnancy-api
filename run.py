0ef4d95b34fccd8926656f4c7b90170cfc192071
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-14 18:48:03 +0100

    Change to localhost in development

--- run.py
+++ run.py
@@ -7,6 +7,13 @@ config_name = os.getenv('CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    if config_name == 'DEVELOPMENT':
+        host = '127.0.0.1'
+        port = 5000
+    else:
+        host = '0.0.0.0'
+        port = 80
+
+    app.run(host=host, port=port)
     # Connect to database at least one to avoid cold start
     get_db()

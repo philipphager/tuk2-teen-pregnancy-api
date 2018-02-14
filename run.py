a59177078cf2febe8db8af531f68a4a12ba042f4
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-14 17:47:16 +0100

    Change host to production

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ config_name = os.getenv('CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
     # Connect to database at least one to avoid cold start
     get_db()

28074001904286fc911c9b86b639104ece1c44c0
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-15 13:42:29 +0100

    Fix world endpoint

--- app/worldwide/service.py
+++ app/worldwide/service.py
@@ -18,7 +18,7 @@ def collect_by_country(data):
     countries = {}
 
     for year in data:
-        for key, value in year:
+        for key, value in year.items():
             if key in countries:
                 countries[key].append(value)
             else:

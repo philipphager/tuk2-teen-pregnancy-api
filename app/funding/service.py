fdc735dd52616b0baf210d217a6c004a40ce120d
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-15 23:38:13 +0100

    Add funding rates

--- app/funding/service.py
+++ app/funding/service.py
@@ -11,6 +11,10 @@ def get_funding_by_state(year):
         data = execute_query(query)
         add_to_cache(query, data)
 
+    for d in data:
+        d['abstinence rate'] = round(d['abstinence only'] / d['total'], 3)
+        d['comprehensive rate'] = round(d['comprehensive sex education'] / d['total'], 3)
+
     return data
 
 
